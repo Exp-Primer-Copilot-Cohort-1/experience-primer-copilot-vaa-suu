91b63fdd26f7226b2e72e5fb30ea99067bfca69e
Vaasu Bhatnagar <v4vaa.suu@gmail.com>
2024-05-23 10:28:14 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillmember() {
+    var member = {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "React", "Node"],
+        details: {
+            address: "Bangalore",
+            pincode: 560016
+        }
+    };
+    return member;
+}
